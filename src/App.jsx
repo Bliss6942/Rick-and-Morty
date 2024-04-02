0f4d2b403acf2ceda0e0fe1282a09c97cb28a3d5
Bliss6942 <2006bekarys@gmail.com>
2024-04-02 14:59:43 +0600

    Убрал отступы

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useState, useEffect } from 'react'
-
 import './App.css'
 
 function App() {
-  
   const [data, setData] = useState()
   const[page,setPage] = useState(1)
   const[isLoading, setLoading] = useState(false)
